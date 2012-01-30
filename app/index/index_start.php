d3e44c02e840a5c164484837cfbbbbc9973ddf78
Muhammad Abrar <abrari@users.noreply.github.com>
2012-01-30 04:16:17 +0000

    Update struktur indeks. Term frequency dibuat field sendiri.

--- app/index/index_start.php
+++ app/index/index_start.php
@@ -11,13 +11,16 @@ include 'trigram.php';
 
 $db = new mysqlDB();
 $db->connect();
- 
+
+// parameter
+$doc_field = "fonetik";
+$index_table = "index";
 
 // fase I : mengekstrak seluruh term dari seluruh dokumen
 echo "Fase I \n\n"; 
  
 // baca seluruh dokumen
-$docs = $db->get_result("SELECT `id`, `fonetik` AS `fonetik` FROM `doc`", "assoc");
+$docs = $db->get_result("SELECT `id`, `{$doc_field}` AS `fonetik` FROM `doc`", "assoc");
 $docs_count = count($docs);
 
 // buat tabel temporer untuk indeks sementara
@@ -33,7 +36,7 @@ foreach ($docs as $doc) {
     $id = $doc['id'];
     $text = $doc['fonetik'];
 
-    echo "Dokumen $id : ";
+    echo "Membaca dokumen $id : ";
         
     // ekstrak trigram
     $trigrams = ekstrak_trigram($text);
@@ -55,26 +58,39 @@ foreach ($docs as $doc) {
 echo "\nFase II \n\n";
 
 // kosongkan dulu tabel indeks
-$db->query("TRUNCATE `index`");
+$db->query("TRUNCATE `{$index_table}`");
 
 // dapatkan seluruh trigram yang ada pada indeks sementara
 $trigram_terms = $db->get_result("SELECT DISTINCT `trigram` FROM `temp_index` ORDER BY `trigram`");
 
 foreach ($trigram_terms as $term) {
     
+    $posting_list = array();
+    $freq_list = array();
+    
     // dapatkan ID dokumen yang memiliki term ini
-    $posting_list = $db->get_result("SELECT `doc_id` FROM `temp_index` WHERE `trigram` = '$term' ORDER BY `doc_id`");
+    $doc_list = $db->get_result("SELECT `doc_id` FROM `temp_index` WHERE `trigram` = '$term' ORDER BY `doc_id`");
+    
+    // hitung frekuensi unik
+    $doc_list = array_count_values($doc_list);
+    ksort($doc_list);
+    
+    foreach ($doc_list as $doc_id => $freq) {
+        $posting_list[] = $doc_id;
+        $freq_list[] = $freq;
+    }
     
     // posting list disimpan dalam bentuk string dipisah koma
     $posting_list_string = implode(",", $posting_list);
+    $freq_list_string = implode(",", $freq_list);
     
     // hitung DF
-    $df = count(array_unique($posting_list)); // dihitung unique karena satu dokumen bisa punya beberapa trigram berulang
+    $df = count($posting_list);
     
-    echo "$term : $df dokumen\n";
+    echo "Term $term : $df dokumen (". substr($posting_list_string, 0, 10) ." -> " . substr($freq_list_string, 0, 10) . ")\n";
     
     // masukkan ke indeks yang sebenarnya
-    $db->query("INSERT INTO `index` (`term`, `df`, `posting_list`) VALUES ('$term', '$df', '$posting_list_string')");
+    $db->query("INSERT INTO `{$index_table}` (`term`, `df`, `posting_list`, `freq_list`) VALUES ('$term', '$df', '$posting_list_string', '$freq_list_string')");
     
 }
 
